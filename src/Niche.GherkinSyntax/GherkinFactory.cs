b3658e069eb0552a222fe6fde926e03b2c0bdbe2
Bevan Arps <bevan.arps@outlook.com>
2018-08-25 12:51:13 +1200

    Update generated documentation

--- src/Niche.GherkinSyntax/GherkinFactory.cs
+++ src/Niche.GherkinSyntax/GherkinFactory.cs
@@ -7,10 +7,17 @@ namespace Niche.GherkinSyntax
     /// <summary>
     /// Entry point for creating Gherkin style test statements
     /// </summary>
-    /// <remarks>
-    /// Recommend use of a <c>using static</c> reference so that the Given method
-    /// can be used directly in test code.
-    /// </remarks>
+    /// <example>
+    /// Import this class via <c>using static</c> so that the methods are directly usable
+    /// without needing to specify <c>GherkinFactory.</c> as a prefix.
+    /// <code>
+    /// using static Niche.GherkinSyntax.GherkinFactory;
+    ///
+    /// [Fact]
+    /// public void CanDoBasicAddition()
+    ///     => Given(ANewCalculator) ...
+    /// </code>
+    /// </example>
     [SuppressMessage(
         "Class Design",
         "AV1008:Class should not be static",
@@ -18,11 +25,15 @@ namespace Niche.GherkinSyntax
     public static class GherkinFactory
     {
         /// <summary>
-        /// Start a scenario test, specifying a function that creates the context for the test
+        /// Start an asynchronous scenario test, specifying a function that creates the context for the test
         /// </summary>
+        /// <remarks>
+        /// See <seealso cref="GivenSyntaxExtensions"/> for extension methods that allow method chaning without needing
+        /// to await the task first.
+        /// </remarks>
         /// <typeparam name="C">Type of context returned.</typeparam>
         /// <param name="createContext">A function to create the test context.</param>
-        /// <returns>A syntax implementation for method chaining.</returns>
+        /// <returns>A task returning a syntax implementation for method chaining.</returns>
         public static async Task<IGivenSyntaxAsync<C>> GivenAsync<C>(
             Func<Task<C>> createContext)
         {
@@ -36,8 +47,13 @@ namespace Niche.GherkinSyntax
         }
 
         /// <summary>
-        /// Start a scenario test, specifying a function that creates the context for the test
+        /// Start an asynchronous scenario test, specifying a function that creates the context for the test and a
+        /// parameter for that function
         /// </summary>
+        /// <remarks>
+        /// See <seealso cref="GivenSyntaxExtensions"/> for extension methods that allow method chaning without needing
+        /// to await the task first.
+        /// </remarks>
         /// <typeparam name="C">Type of context returned.</typeparam>
         /// <typeparam name="P">Type of parameter passed.</typeparam>
         /// <param name="createContext">A function to create the test context.</param>
